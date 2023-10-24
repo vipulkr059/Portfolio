15a0a90e8c515407db3b232f4340969d239e89ad
vipulkr059 <vipulkr059@gmail.com>
2023-10-25 00:06:56 +0530

    deploy

--- src/components/skills/Circle.jsx
+++ src/components/skills/Circle.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { Cir } from "./circleStyle";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+// import React from "react";
+// import { Cir } from "./circleStyle";
+//  import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
-import style from "./style.css";
-import VisibilitySensor from "react-visibility-sensor";
+// import style from "./style.css";
+// import VisibilitySensor from "react-visibility-sensor";
 
-export default function Circle({ p, skill }) {
-  return (
-    <Cir>
-      <VisibilitySensor>
-        {({ isVisible }) => {
-          const percentage = isVisible ? p : 0;
-          return (
-            <div
-              className="circleContainer"
-              data-aos="fade-right"
-              data-aos-duration="1000"
-            >
-              <div className="heading">{skill}</div>
-              <CircularProgressbar value={percentage} text={`${percentage}%`} />
-            </div>
-          );
-        }}
-      </VisibilitySensor>
-    </Cir>
-  );
-}
+// export default function Circle({ p, skill }) {
+//   return (
+//     <Cir>
+//       <VisibilitySensor>
+//         {({ isVisible }) => {
+//           const percentage = isVisible ? p : 0;
+//           return (
+//             <div
+//               className="circleContainer"
+//               data-aos="fade-right"
+//               data-aos-duration="1000"
+//             >
+//               <div className="heading">{skill}</div>
+//               <CircularProgressbar value={percentage} text={`${percentage}%`} />
+//             </div>
+//           );
+//         }}
+//       </VisibilitySensor>
+//     </Cir>
+//   );
+// }
